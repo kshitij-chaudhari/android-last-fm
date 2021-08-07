426b0261a263f4916336a4bb7496d49681f5ac1e
Kshitij Chaudhari <jitihsk@gmail.com>
2021-08-07 23:10:50 +0100

    Toast is now shown on error

--- lastfm-app/src/main/java/com/kc/android/lastfm/ui/feature/albums/list/AlbumsListScreen.kt
+++ lastfm-app/src/main/java/com/kc/android/lastfm/ui/feature/albums/list/AlbumsListScreen.kt
@@ -3,6 +3,7 @@
  */
 package com.kc.android.lastfm.ui.feature.albums.list
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,11 +18,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import com.kc.android.lastfm.Destinations
@@ -42,7 +45,10 @@ fun AlbumsListScreen(
         topBar = {
             Surface(elevation = 8.dp) {
                 Box(
-                    Modifier.padding(16.dp).background(MaterialTheme.colors.background).fillMaxWidth()
+                    Modifier
+                        .padding(16.dp)
+                        .background(MaterialTheme.colors.background)
+                        .fillMaxWidth()
                 ) {
                     var searchQuery by remember { mutableStateOf(TextFieldValue()) }
                     val focusManager = LocalFocusManager.current
@@ -58,7 +64,22 @@ fun AlbumsListScreen(
             }
         }
     ) {
+        val context = LocalContext.current
         val albums = albumsListViewModel.albums.collectAsLazyPagingItems()
+
+        val errorState = when {
+            albums.loadState.append is LoadState.Error -> albums.loadState.append as LoadState.Error
+            albums.loadState.prepend is LoadState.Error -> albums.loadState.prepend as LoadState.Error
+            albums.loadState.refresh is LoadState.Error -> albums.loadState.refresh as LoadState.Error
+            else -> null
+        }
+        errorState?.let {
+            Toast.makeText(
+                context,
+                it.error.message ?: stringResource(id = R.string.search_error),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
         if (albums.itemCount == 0) {
             FullScreenCircularLoadingIndicator()
         } else {
